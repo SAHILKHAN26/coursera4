1f523977d67b8cce2849f6bbe14c03556d0d5e8d
Sahil khan <64180078+SAHILKHAN26@users.noreply.github.com>
2020-06-09 16:41:50 +0530

    SpeakGoodBye.js

--- Container/Harder/SpeakGoodBye.js
+++ Container/Harder/SpeakGoodBye.js
@@ -3,18 +3,18 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
-/*(function (window) {
+(function (window) {
 var byeSpeaker = {
 		speak: function(name) {
 			console.log(speakWord + " " + name);
-		}
+		};
 	}
 
 	var speakWord = "Good Bye";
 
 	window.byeSpeaker = byeSpeaker;
 
-})(window);*/
+})(window);
 
 	
 
